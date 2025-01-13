32129c54ab7b3251bf8c4dedc282800426a00d36
Joao Lisboa <90469235+joaovclisboa@users.noreply.github.com>
2025-01-13 12:11:56 -0300

    Create estender_base_ortogonal.py

---
+++ estender_base_ortogonal.py
@@ -0,0 +1,11 @@
+from sympy import Matrix, zeros, linsolve, S
+#Vamos supor duas coisas
+
+#Seja W um subespaço de V e suponha que W possui uma base B = {w1, ...,ws}. 
+#Podemos estender B para uma base ortogonal de V.
+
+#V possui uma base 
+
+
+#Primeiramente, temos que receber os vetores de B e verificar se
+#eles sao ortogonais. Se nao forem, teremos que ortogonalizar esses vetores.
